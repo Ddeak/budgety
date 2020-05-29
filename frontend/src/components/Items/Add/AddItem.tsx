451d410ff1fd63c0c8695d5caf0215df9ab6251a
Daniel Deak <ddeak@insights.com>
2020-05-29 21:28:00 +0100

    Hooked up create-item call to use the create-item mutation.

--- frontend/src/components/Items/Add/AddItem.tsx
+++ frontend/src/components/Items/Add/AddItem.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
 import AddItemView from './AddItemView';
+import { IItem } from '../../../types/item';
+
+const ADD_ITEM_MUTATION = gql`
+  mutation SaveITem($name: String!, $category: String!, $price: String!) {
+    saveItem(name: $name, category: $category, price: $price) {
+      name
+      category
+      price
+    }
+  }
+`;
 
 const AddItem = () => {
-  const createItem = () => {};
+  const [addItem] = useMutation(ADD_ITEM_MUTATION);
+  const createItem = (item: IItem) => {
+    addItem({ variables: { ...item } });
+  };
 
   return <AddItemView createItem={createItem} />;
 };
