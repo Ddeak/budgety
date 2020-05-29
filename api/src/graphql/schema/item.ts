09353dc4d94c8b565e5922c03674bb344ba253c1
Daniel Deak <ddeak@insights.com>
2020-05-29 19:56:20 +0100

    Added first GraphQL query from frontend to backend.

--- api/src/graphql/schema/item.ts
+++ api/src/graphql/schema/item.ts
@@ -1,20 +1,21 @@
-import { gql } from "apollo-server-micro";
+import { gql } from 'apollo-server-micro';
 
 export default gql`
-    extend type Query {
-        getAllItems: [Item!]
-        getItem(_id: ID!): Item
-    }
+  extend type Query {
+    getAllItems: [Item!]
+    getItem(_id: ID!): Item
+  }
 
-    extend type Mutation {
-        saveItem(name: String!, category: String!, price: String!): Item!
-        deleteItem(_id: ID!): Item
-    }
+  extend type Mutation {
+    saveItem(name: String!, category: String!, price: String!): Item!
+    deleteItem(_id: ID!): Item
+  }
 
-    type Item {
-        _id: ID!
-        name: String!
-        category: String!
-        date: DateTime!
-    }
+  type Item {
+    _id: ID!
+    name: String!
+    category: String!
+    price: String!
+    date: DateTime!
+  }
 `;
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 
 import Typography from '@material-ui/core/Typography';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const ALL_ITEMS = gql`
+  {
+    getAllItems {
+      name
+      category
+      price
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, error, data } = useQuery(ALL_ITEMS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log('thisfar', data);
   return (
     <div>
       <Typography variant="h1">Home Page</Typography>
